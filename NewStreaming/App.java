87f11f2b13ec300f580babc4b95aed943dc235a4
tsilavina007 <andriamiart114@gmail.com>
2024-08-07 22:48:05 +0300

    feat: add User class

---
+++ NewStreaming/App.java
@@ -0,0 +1,7 @@
+package NewStreaming;
+
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ NewStreaming/User.java
@@ -0,0 +1,13 @@
+package NewStreaming;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+
+    public User(String firstName, String lastName, String birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+}
